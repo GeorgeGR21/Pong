cc3a9b412371629866aa738856569b9439715820
George Dam <123986260+GeorgeGR21@users.noreply.github.com>
2023-02-13 23:39:50 +0200

    Big improvements

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,6 +20,14 @@ public class GameManager : MonoBehaviour
     public GameObject Player1Text;
     public GameObject Player2Text;
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
     private int Player1Score;
     private int Player2Score;
 
@@ -34,10 +42,10 @@ public class GameManager : MonoBehaviour
             audio.PlayOneShot(audio.clip);
         }
         ResetPosition();
-    }
+     }
 
-    public void Player2Scored()
-    {
+        public void Player2Scored()
+        {
         Player2Score++;
         Player2Text.GetComponent<TextMeshProUGUI>().text = Player2Score.ToString();
         ball.GetComponent<Ball>().speed += 1;
@@ -47,7 +55,7 @@ public class GameManager : MonoBehaviour
             audio.PlayOneShot(audio.clip);
         }
         ResetPosition();
-    }
+        }
 
     private void ResetPosition()
     {
